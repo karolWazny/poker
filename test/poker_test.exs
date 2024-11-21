8ae3272994fd36928b7c221a425768c7f3907694
Karol Ważny <karol.wazny@orange.com>
2024-11-21 08:45:45 +0100

    case tests

--- test/poker_test.exs
+++ test/poker_test.exs
@@ -2,7 +2,14 @@ defmodule PokerTest do
   use ExUnit.Case
   doctest Poker
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck makes 52 cards" do
+    deck_length = length(Poker.create_deck)
+    assert deck_length == 52
   end
+
+  test "shuffling a deck randomizes it" do
+    deck = Poker.create_deck
+    refute deck == Poker.shuffle(deck)
+  end
+
 end
