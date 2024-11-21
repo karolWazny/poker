a52f05d02534d13742c4403febf600efc20c98ab
Karol Ważny <karol.wazny@orange.com>
2024-11-21 08:40:33 +0100

    doctests

--- lib/poker.ex
+++ lib/poker.ex
@@ -13,10 +13,33 @@ defmodule Poker do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Determines whether a deck contains a given card.
+
+  ## Examples
+
+      iex> deck = Poker.create_deck
+      iex> Poker.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Divides a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should
+  be in the hand.
+
+  ## Examples
+
+      iex> deck = Poker.create_deck
+      iex> {hand, _deck} = Poker.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
